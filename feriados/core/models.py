a4dc2824f8b089090d199b9006af6fb2232d0679
endeu <wendellpguariento@gmail.com>
2022-06-15 19:17:13 -0300

    adicionando banco de dados em models

--- feriados/core/models.py
+++ feriados/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class FeriadoModel(models.Model):
+    nome = models.CharField('Feriado',max_length=50)
+    dia = models.IntegerField('Data')
+    mes = models.IntegerField('Mês')
+
+def __str__(self):
+    return self.nome
